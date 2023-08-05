91d657d6e92cfa4846e17388cb9ad3fcb2fd26ff
prashantkr2601 <prashantkr2601@gmail.com>
2023-08-06 03:27:14 +0530

    modified footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   cardImgeData: any = [];
+  footerData: any = [];
+
   constructor() {
+    this.footerData = [
+      {
+        heading: 'Jobseekers',
+        subHeading: [
+          'Register & Upload CV',
+          'Browse Jobs by location',
+          'Newest Jobs',
+          'Blog',
+          'Help',
+        ],
+      },
+      {
+        heading: 'Recruiters',
+        subHeading: [
+          'Advertise',
+          'Login',
+          'Associations',
+          'Recruiteng? <span>Call us on 01772639605</span>',
+        ],
+      },
+      {
+        heading: 'Stay in touch',
+        subHeading: [
+          'Contact Details',
+          'Sitemap',
+          'GDPR',
+          'Privacy Policy',
+          `Terms & Conditions`,
+        ],
+      },
+    ];
+
     this.cardImgeData = [
       'https://cdn.testbook.com/resources/productionimages/Varinder%20Grover_All_1658412204.png',
       'https://cdn.testbook.com/resources/productionimages/Varinder%20Grover_All_1658412204.png',
@@ -16,5 +50,6 @@ export class AppComponent {
       'https://cdn.testbook.com/resources/productionimages/Varinder%20Grover_All_1658412204.png',
     ];
   }
+
   title = 'test-practice';
 }
